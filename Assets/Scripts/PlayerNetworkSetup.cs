55a2bba71368a38d64cc8787c1e9ac630ef68d63
ehnree <hzhou1995@gmail.com>
2015-08-09 15:51:30 -0400

    multiplayer!

---
+++ Assets/Scripts/PlayerNetworkSetup.cs
@@ -0,0 +1,25 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerNetworkSetup : NetworkBehaviour {
+	[SerializeField] Camera FPSCharacterCam;
+	[SerializeField] AudioListener AudioListener;
+
+	// Use this for initialization
+	void Start () {
+		if (isLocalPlayer) {
+			GameObject.Find ("Main Camera").SetActive(false);
+			GetComponent<CharacterController> ().enabled = true;
+			GetComponent<UnityStandardAssets.Characters.FirstPerson.FirstPersonController> ().enabled = true;
+			FPSCharacterCam.enabled = true;
+			AudioListener.enabled = true;
+		}
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
